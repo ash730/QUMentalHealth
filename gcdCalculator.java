7c8b0c018657389d00cc62759e463b23b9cca9ac
JHandy24 <jmhandy@quinnipiac.edu>
2021-03-16 08:47:27 -0400

    Adds a class that helps stressed out Discrete math students by finding the GCD of two numbers

---
+++ gcdCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class gcdCalculator {
+
+	public static void main(String[] args) {
+		
+		int num1, num2;
+		
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.print("Enter first number:");
+        num1 = (int)myScanner.nextInt();
+        
+        System.out.print("Enter second number:");
+        num2 = (int)myScanner.nextInt();
+        
+        myScanner.close();
+        
+        while (num1 != num2) {
+        	if(num1 > num2)
+                num1 = num1 - num2;
+            else
+                num2 = num2 - num1;
+        }
+        
+        System.out.printf("GCD of given numbers is: %d", num2);
+	}
+}
